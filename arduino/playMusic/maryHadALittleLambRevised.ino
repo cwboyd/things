a30b7952da10b6d7dc3c4d7e309f6f02d10648f7
cwboyd <cwboyd@users.noreply.github.com>
2019-05-19 09:29:59 -0500

    Revised version, uses arrays of structs instead of parallel arrays

--- arduino/playMusic/maryHadALittleLambRevised.ino
+++ arduino/playMusic/maryHadALittleLambRevised.ino
@@ -23,95 +23,68 @@ This example code is in the public domain.
 //int noteDurations[] = { 4, 8, 8, 4,4,4,4,4 };
 
 
-int melody[] = { 
-  // Line 1
-  NOTE_B3, NOTE_A3, NOTE_G3, NOTE_A3,
-  NOTE_B3, NOTE_B3, NOTE_B3,
-  NOTE_A3, NOTE_A3, NOTE_A3,
-  NOTE_B3, NOTE_D4, NOTE_D4,
-  // Line 2
-  NOTE_B3, NOTE_A3, NOTE_G3, NOTE_A3,
-  NOTE_B3, NOTE_B3, NOTE_B3, NOTE_B3,
-  NOTE_A3, NOTE_A3, NOTE_B3, NOTE_A3,
-  NOTE_G3,  
-  // Line 3
-  NOTE_B3, NOTE_A3, NOTE_G3, NOTE_A3,
-  NOTE_B3, NOTE_B3, NOTE_B3,
-  NOTE_A3, NOTE_A3, NOTE_A3,
-  NOTE_B3, NOTE_D4, NOTE_D4,
-  // Line 4
-  NOTE_B3, NOTE_A3, NOTE_G3, NOTE_A3,
-  NOTE_B3, NOTE_B3, NOTE_B3, NOTE_B3,
-  NOTE_A3, NOTE_A3, NOTE_B3, NOTE_A3,
-  NOTE_G3, NOTE_D4, 
-  // Line 5
-  NOTE_B3, NOTE_A3, NOTE_G3, NOTE_A3,
-  NOTE_B3, NOTE_B3, NOTE_B3,
-  NOTE_A3, NOTE_A3, NOTE_A3,
-  NOTE_B3, NOTE_D4, NOTE_D4, NOTE_D4,
-  // Line 6
-  NOTE_B3, NOTE_A3, NOTE_G3, NOTE_A3,
-  NOTE_B3, NOTE_B3, NOTE_B3, NOTE_B3,
-  NOTE_A3, NOTE_A3, NOTE_B3, NOTE_A3,
-  NOTE_G3, NOTE_D4, 
-  // Line 7
-  NOTE_B3, NOTE_A3, NOTE_G3, NOTE_A3,
-  NOTE_B3, NOTE_B3, NOTE_B3,
-  NOTE_A3, NOTE_A3, NOTE_A3,
-  NOTE_B3, NOTE_D4, NOTE_D4, NOTE_D4,
-  // Line 8
-  NOTE_B3, NOTE_A3, NOTE_G3, NOTE_A3,
-  NOTE_B3, NOTE_B3, NOTE_B3, NOTE_B3,
-  NOTE_A3, NOTE_A3, NOTE_B3, NOTE_A3,
-  NOTE_G3,  
+struct Note
+{
+   int note;
+   float duration;
 };
 
-float noteDurations[] = { 
+Note melody[] =
+{
   // Line 1
-  4, 4, 4, 4,
-  4, 4, 2,
-  4, 4, 2,
-  4, 4, 2,
+  { NOTE_B3, 4.0 }, { NOTE_A3, 4.0 }, { NOTE_G3, 4.0 }, { NOTE_A3, 4.0 },
+  { NOTE_B3, 4.0 }, { NOTE_B3, 4.0 }, { NOTE_B3, 2.0 },
+  { NOTE_A3, 4.0 }, { NOTE_A3, 4.0 }, { NOTE_A3, 2.0 },
+  { NOTE_B3, 4.0 }, { NOTE_D4, 4.0 }, { NOTE_D4, 2.0 },
+
   // Line 2
-  4, 4, 4, 4,
-  4, 4, 4, 4,
-  4, 4, 4, 4,
-  1,
+  { NOTE_B3, 4.0 }, { NOTE_A3, 4.0 }, { NOTE_G3, 4.0 }, { NOTE_A3, 4.0 },
+  { NOTE_B3, 4.0 }, { NOTE_B3, 4.0 }, { NOTE_B3, 4.0 }, { NOTE_B3, 4.0 },
+  { NOTE_A3, 4.0 }, { NOTE_A3, 4.0 }, { NOTE_B3, 4.0 }, { NOTE_A3, 4.0 },
+  { NOTE_G3, 1.0 },
+
   // Line 3
-  4, 4, 4, 4,
-  4, 4, 2,
-  4, 4, 2,
-  4, 4, 2,
+  { NOTE_B3, 4.0 }, { NOTE_A3, 4.0 }, { NOTE_G3, 4.0 }, { NOTE_A3, 4.0 },
+  { NOTE_B3, 4.0 }, { NOTE_B3, 4.0 }, { NOTE_B3, 2.0 },
+  { NOTE_A3, 4.0 }, { NOTE_A3, 4.0 }, { NOTE_A3, 2.0 },
+  { NOTE_B3, 4.0 }, { NOTE_D4, 4.0 }, { NOTE_D4, 2.0 },
+
   // Line 4
-  4, 4, 4, 4,
-  4, 4, 4, 4,
-  4, 4, 4, 4,
-  1.3, 4,
+  { NOTE_B3, 4.0 }, { NOTE_A3, 4.0 }, { NOTE_G3, 4.0 }, { NOTE_A3, 4.0 },
+  { NOTE_B3, 4.0 }, { NOTE_B3, 4.0 }, { NOTE_B3, 4.0 }, { NOTE_B3, 4.0 },
+  { NOTE_A3, 4.0 }, { NOTE_A3, 4.0 }, { NOTE_B3, 4.0 }, { NOTE_A3, 4.0 },
+  { NOTE_G3, 1.3 }, { NOTE_D4, 4.0 },
+ 
   // Line 5
-  4, 4, 4, 4,
-  4, 4, 2,
-  4, 4, 2,
-  4, 4, 4, 4,
+  { NOTE_B3, 4.0 }, { NOTE_A3, 4.0 }, { NOTE_G3, 4.0 }, { NOTE_A3, 4.0 },
+  { NOTE_B3, 4.0 }, { NOTE_B3, 4.0 }, { NOTE_B3, 2.0 },
+  { NOTE_A3, 4.0 }, { NOTE_A3, 4.0 }, { NOTE_A3, 2.0 },
+  { NOTE_B3, 4.0 }, { NOTE_D4, 4.0 }, { NOTE_D4, 4.0 }, { NOTE_D4, 4.0 },
+
   // Line 6
-  4, 4, 4, 4,
-  4, 4, 4, 4,
-  4, 4, 4, 4,
-  1.3, 4,
+  { NOTE_B3, 4.0 }, { NOTE_A3, 4.0 }, { NOTE_G3, 4.0 }, { NOTE_A3, 4.0 },
+  { NOTE_B3, 4.0 }, { NOTE_B3, 4.0 }, { NOTE_B3, 4.0 }, { NOTE_B3, 4.0 },
+  { NOTE_A3, 4.0 }, { NOTE_A3, 4.0 }, { NOTE_B3, 4.0 }, { NOTE_A3, 4.0 },
+  { NOTE_G3, 1.3 }, { NOTE_D4, 4.0 },
+
   // Line 7
-  4, 4, 4, 4,
-  4, 4, 2,
-  4, 4, 2,
-  4, 4, 4, 4,
+  { NOTE_B3, 4.0 }, { NOTE_A3, 4.0 }, { NOTE_G3, 4.0 }, { NOTE_A3, 4.0 },
+  { NOTE_B3, 4.0 }, { NOTE_B3, 4.0 }, { NOTE_B3, 2.0 },
+  { NOTE_A3, 4.0 }, { NOTE_A3, 4.0 }, { NOTE_A3, 2.0 },
+  { NOTE_B3, 4.0 }, { NOTE_D4, 4.0 }, { NOTE_D4, 4.0 }, { NOTE_D4, 4.0 },
+
   // Line 8
-  4, 4, 4, 4,
-  4, 4, 4, 4,
-  4, 4, 4, 4,
-  1,
-};  
-  
+  { NOTE_B3, 4.0 }, { NOTE_A3, 4.0 }, { NOTE_G3, 4.0 }, { NOTE_A3, 4.0 },
+  { NOTE_B3, 4.0 }, { NOTE_B3, 4.0 }, { NOTE_B3, 4.0 }, { NOTE_B3, 4.0 },
+  { NOTE_A3, 4.0 }, { NOTE_A3, 4.0 }, { NOTE_B3, 4.0 }, { NOTE_A3, 4.0 },
+  { NOTE_G3, 1.0 },
+};
+
+
 // calculte number of notes by dividing size of melody array (which gives
 // bytes) divided by size of an integer (also in bytes).
-int numberOfNotes = sizeof(melody) / sizeof(int);
+//int numberOfNotes = sizeof(melody) / sizeof(int);
+int numberOfNotes = sizeof(melody) / sizeof(Note);
 
 void setup() 
 {
@@ -122,11 +95,14 @@ void setup()
   // iterate over the notes of the melody:
   for (int thisNote = 0; thisNote < numberOfNotes; thisNote++) 
   {
+    Note& note = melody[thisNode];
+
     // to calculate the note duration, take one second 
     // divided by the note type.
     //e.g. quarter note = 1000 / 4, eighth note = 1000/8, etc.
-    int noteDuration = 1000/noteDurations[thisNote];
-    tone(8, melody[thisNote],noteDuration);
+    //int noteDuration = 1000/noteDurations[thisNote];
+    int noteDuration = 1000/melody[thisNode].duration;
+    tone(8, melody[thisNote].note, noteDuration);
 
     // to distinguish the notes, set a minimum time between them.
     // the note's duration + 30% seems to work well:
@@ -144,3 +120,4 @@ void loop()
   digitalWrite(13, LOW);    // set the LED off
   delay(1000);              // wait for a second
 }
+
